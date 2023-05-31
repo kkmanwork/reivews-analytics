59776297735cf45019a18790904f38aa7b1bd6cf
kkmanwork <jabawork015@gmail.com>
2023-05-31 14:30:22 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -3,6 +3,9 @@ count = 0
 with open("reviews.txt", "r") as f:
 	for line in f:
 		data.append(line)
-		count +=1 
-		if count % 1000 == 0:
-			print(len(data))
+
+sum_len = 0
+for d in data: 
+	sum_len += len(d)
+print("留言的平均長度:", sum_len / len(data))
+
