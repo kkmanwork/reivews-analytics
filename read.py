6830659112e7af6751d1805afd8d4e3a83207a29
kkmanwork <jabawork015@gmail.com>
2023-05-31 15:02:25 +0800

    add good version

--- read.py
+++ read.py
@@ -14,3 +14,9 @@ for d in data:
 	if len(d) <= 100:
 		new.append(d)
 print("一共有",len(new), "筆留言長度小於等於100")
+
+good = []
+for d in data:
+	if "good" in d:
+		good.append(d)
+print("一共有", len(good), "筆留言提到good")
